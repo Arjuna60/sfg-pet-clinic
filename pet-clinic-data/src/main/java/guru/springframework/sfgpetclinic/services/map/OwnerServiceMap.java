c01b80a14fc3128da33529004e2de7f2eae0b6a1
Arjuna60 <arjuna@admin-vedatech.com>
2018-10-09 15:56:28 -0500

    Implements Base Service created closes #6

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -6,24 +6,25 @@ import guru.springframework.sfgpetclinic.services.CrudService;
 import java.util.Set;
 
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+
     @Override
     public Set<Owner> findAll() {
         return super.findAll();
     }
 
     @Override
-    public void delete(Owner object) {
-
-    }
-
-    @Override
     public Owner save(Owner object) {
-        return super.save(object);
+        return super.save(object.getId(), object);
     }
 
     @Override
-    public void deletById(Long aLong) {
-        delete(aLong);
+    public void delete(Owner object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
     }
 
     @Override
